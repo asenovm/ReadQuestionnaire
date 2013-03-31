ce37dd5dbfbb5c4ec3f447c0cbbe486a53ab9803
martin <asenov.m@gmail.com>
2013-03-31 19:07:14 +0300

    added an answer field + submitting button

--- ReadQuestionnaire/QuestionnaireForm.Designer.cs
+++ ReadQuestionnaire/QuestionnaireForm.Designer.cs
@@ -29,23 +29,46 @@
         private void InitializeComponent()
         {
             this.questionTitle = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nextQuestionButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // questionTitle
             // 
-            this.questionTitle.AutoSize = true;
-            this.questionTitle.Location = new System.Drawing.Point(351, 42);
+            this.questionTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.questionTitle.Location = new System.Drawing.Point(33, 32);
             this.questionTitle.Name = "questionTitle";
-            this.questionTitle.Size = new System.Drawing.Size(49, 13);
+            this.questionTitle.Size = new System.Drawing.Size(716, 101);
             this.questionTitle.TabIndex = 0;
             this.questionTitle.Text = "Question";
             this.questionTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(711, 317);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Моля отговорете на въпроса с не по-малко от 1000 символа";
+            // 
+            // nextQuestionButton
+            // 
+            this.nextQuestionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextQuestionButton.Location = new System.Drawing.Point(306, 461);
+            this.nextQuestionButton.Name = "nextQuestionButton";
+            this.nextQuestionButton.Size = new System.Drawing.Size(202, 39);
+            this.nextQuestionButton.TabIndex = 2;
+            this.nextQuestionButton.Text = "Следващ въпрос";
+            this.nextQuestionButton.UseVisualStyleBackColor = true;
             // 
             // MainContainer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(781, 512);
+            this.Controls.Add(this.nextQuestionButton);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.questionTitle);
             this.Name = "MainContainer";
             this.Text = "Read Questionnaire";
@@ -57,6 +80,8 @@
         #endregion
 
         private System.Windows.Forms.Label questionTitle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button nextQuestionButton;
     }
 }
 
